5acbf46ed5cdf49f61df04b88833c86572a0f244
rico7c <rico7c@gmail.com>
2013-07-30 13:53:00 +0100

    update
    updating on Monster.js

--- Monsters.js
+++ Monsters.js
@@ -3,25 +3,33 @@ var dragons = [];
 dragons.black = {
 	name: "Onyx",
 	dmg: 50,
-	HP: 1000
+	HP: 1000,
+   level: 0,
+   image: "source"
 };
 
 dragons.red = {
    name: "Beryls"
    dmg: 25
-   HP:500
+   HP:500,
+   level: 0,
+   image: "source"
  };
 
 dragons.blue = {
    name: "Lazuli"
    dmg: 15
-   HP:300
+   HP:300,
+   level: 0,
+   image: "source"
  };
 
 dragons.green = {
    name: "Naphrite"
    dmg: 10
-   HP:200
+   HP:200,
+   level: 0,
+   image: "source"
  };
 
  var zombie = [];
@@ -29,19 +37,25 @@ dragons.green = {
  zombie.undead = {
  	name: "Undead",
  	dmg: 5,
- 	HP: 50
+ 	HP: 50,
+   level: 0,
+   image: "source"
  };
 
  zombie.dying = {
  	name: "Dying",
- 	dmg: 3
- 	HP:30
+ 	dmg: 3,
+ 	HP:30,
+   level: 0,
+   image: "source"
  };
 
  Zombie.rotting = {
  	name: "Rotting",
  	dmg:1,
- 	Hp:10
+ 	Hp:10,
+   level: 0,
+   image: "source"
  };
 
  var bear = [];
@@ -49,31 +63,237 @@ dragons.green = {
  bear.black = {
  	name: "Black bear",
  	dmg: 15,
- 	HP: 150
+ 	HP: 150,
+   level: 0,
+   image: "source"
  };
 
 bear.brown = {
  	name: "Brwon bear",
  	dmg: 10,
- 	HP: 100
+ 	HP: 100,
+   level: 0,
+   image: "source"
  };
 
 bear.grey = {
  	name: "Grey bear",
  	dmg: 5,
- 	HP: 50
- }
- var Giant = []; //warrior,
- var skelenton = []; //Giant, warrior
+ 	HP: 50,
+   level: 0,
+   image: "source"
+ };
+
+ var skelenton = [];
+
+ skelenton.warrior = {
+   name: "Skelenton Warrior",
+   dmg: 50,
+   HP: 500,
+   level: 0,
+   image: "source"
+ };
+
+ skelenton.bonecruncher = {
+   name: "Bone Cruncher skelenton",
+   dmg: 25,
+   HP: 250,
+   level: 0,
+   image: "source"
+ };
+
  var orc = [];
+
+ orc.warrior = {
+   name: "Orc Warrior",
+   dmg: 25,
+   HP: 250,
+   level: 0,
+   image: "source"
+ };
+
+orc.blood = {
+   name: "Blood Orc",
+   dmg: 50,
+   HP: 500,
+   level: 0,
+   image: "source"
+};
+
+orc.giant = {
+   name: "Giant Orc",
+   dmg: 100,
+   HP: 1000,
+   level: 0,
+   image: "source"
+};
+
  var witch = [];
+
+ witch.fire - {
+   name: "Fire Witch",
+   dmg: 25,
+   HP: 250,
+   level: 0,
+   image: "source"
+};
+
+witch.earth = {
+   name: "Earth Witch",
+   dmg: 20,
+   HP: 200,
+   level: 0,
+   image: "source"
+};
+
+witch.air = {
+   name: "Air Witch",
+   dmg: 15,
+   HP: 150,
+   level: 0,
+   image: "source"
+};
+
  var vampire = [];
+
+ vampire.dracular = {
+   name: "Dracular",
+   dmg: 100,
+   HP: 1500,
+   level: 0,
+   image: "source"
+ };
+
+ vampire.minon = {
+   name: "Vampire minon",
+   dmg: 30,
+   HP: 300,
+   level: 0,
+   image: "source"
+};
+
  var rat = [];
- var bats = [];
+
+ rat.giant = {
+   name: "Giant Rat",
+   dmg: 20,
+   HP: 200,
+   level: 0,
+   image: "source"
+};
+
+rat.crazed = {
+   name: "Crazed Rat",
+   dmg: 30,
+   HP: 200,
+   level: 0,
+   image: "source"
+};
+
+rat.young = {
+   name: "Young Rat",
+   dmg: 5,
+   HP: 50,
+   level: 0,
+   image: "source"
+};
+
+ var bat = [];
+
+ bat.blood = {
+   name: "Blood Bat",
+   dmg: 75,
+   HP: 750,
+   level: 0,
+   image: "source"
+ };
+
+ bat.vampire = {
+   name: "Vampire Bat",
+   dmg: 40,
+   HP: 400,
+   level: 0,
+   image: "source"
+};
+
+bat.giant = {
+   name: "Giant Bat",
+   dmg: 20,
+   HP: 200,
+   level: 0,
+   image: "source"
+};
+
  var spider = [];
+
+ spider.giant = {
+   name: "Giant Spider",
+   dmg: 35,
+   HP: 350,
+   level: 0,
+   image: "source"
+ };
+
+spider.venom = {
+   name: "Poisonous Spider",
+   dmg: 45,
+   HP: 450,
+   level: 0,
+   image: "source"
+};
+
+spider.young = {
+   name: "Young Spider",
+   dmg: 10,
+   HP: 150,
+   level: 0,
+   image: "source"
+};
+
  var wolf = [];
+
+ wolf.alfa = {
+   name: "Alfa Wolf",
+   dmg: 80,
+   HP: 800,
+   level: 0,
+   image: "source"
+ }
+
+ wolf.blood = {
+   name: "Blood Wolf",
+   dmg: 65,
+   HP: 650,
+   level: 0,
+   image: "source"
+ };
+
+ wold.pup = {
+   name: "Wolf Pup",
+   dmg: 20,
+   HP: 200,
+   level: 0,
+   image: "source"
+};
+
  var scorpion = [];
 
- //add moves/attacks
- //add level/requirements
+ scorpion.blood = {
+   name: "Blood Scorpion",
+   dmg: 60,
+   HP: 600,
+   level: 0,
+   image: "source"
+};
+
+scorpion.venom = {
+   name: "Poisonous Scorpion",
+   dmg: 50,
+   HP: 500,
+   level: 0,
+   image: "source"
+}
+
+ //add moves/attacks/magic
+ //add requirements
  //specials
